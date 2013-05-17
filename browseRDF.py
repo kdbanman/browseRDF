d3c7ac0a61e6036dad26a763007a31cf4a66bb19
kdbanman <kdbanman@ualberta.ca>
2013-05-17 11:55:06 -0600

    factor view into main file.

--- browseRDF.py
+++ browseRDF.py
@@ -4,9 +4,6 @@
 #
 # This code is subject to the GPLv3 license.
 
-import sys
-import os
-
 import tulip
 import tulipgui
 
@@ -14,17 +11,7 @@ import model
 import view
 import controller 
 
-# mute all stderr unless requested by 'errors' parameter.
-# this is at the beginning because if the sterr stream is muted anywhere else,
-# the muting doesn't work at all.
-ARG = sys.argv
-if not (len(ARG) == 2 and ARG[1] == 'debug'):
-  r,w = os.pipe()
-  os.close(sys.stderr.fileno())
-  os.dup2(w, sys.stderr.fileno())
-
 mod = model.Model()
-vw = view.View()
 cont = controller.Controller()
 
 
@@ -32,6 +19,28 @@ exploring = True
 showLabels = False
 nbrClicked = []
 
+
+def newFrontier(self, graph, labels):
+  '''
+  "graph" is a tlp.Graph()
+  newFrontier creates a new window with the contents of a freshly
+  dereferenced uri.  It's meant to be called after the controller has
+  converted a retrieved rdflib graph into a tulip graph.
+  '''
+  view = tulipgui.tlp.addNodeLinkDiagramView(graph)
+  view.setOptionsWidgetsVisible(False)
+
+  params = view.getRenderingParameters()
+  params.setViewEdgeLabel(labels)
+  params.setViewNodeLabel(labels)
+  params.setEdgeSizeInterpolate(False)
+  params.setViewArrow(True)
+  params.setLabelsDensity(0)
+  params.setLabelScaled(False)
+  params.setNodesLabelStencil(0)
+  params.setMinSizeOfLabel(15)
+
+  view.setRenderingParameters(params)
 
 def arrangeViews():
   i = 0
@@ -66,7 +75,7 @@ class SelectObserver(tulip.tlp.PropertyObserver):
       try:
         rdfGraph = mod.getGraph(uri)
         tlpGraph = cont.triplesToTulip(rdfGraph)
-        vw.newFrontier(tlpGraph, showLabels)
+        newFrontier(tlpGraph, showLabels)
 	arrangeViews()
       except:
         print "RDF document not returned from " + uri
@@ -118,62 +127,58 @@ def displayLabels(show):
     v.setRenderingParameters(params)
 
 
-if len(ARG) == 1 or len(ARG) == 2 or len(ARG) == 2 and ARG[1] == 'debug':
-
-  if len(ARG) == 1 or ARG[1] == "debug":
+def explore(uri=None):
+  if not uri:
     uri = "http://dbpedia.org/data/Albert_Einstein"
     #uri = "http://rdf.freebase.com/ns/en.germany"
     #uri = "http://xmlns.com/foaf/spec/index.rdf"
     #uri = "http://purl.org/dc/elements/1.1/"
     #uri = "IT_Vocabulary.rdf"
-  else:
-    uri = ARG[1]
   
   try:
     rdfGraph = mod.getGraph(uri)
     tlpGraph = cont.triplesToTulip(rdfGraph)
-    vw.newFrontier(tlpGraph, showLabels)
+    newFrontier(tlpGraph, showLabels)
     arrangeViews()
   except:
     print "ERROR:  RDF not returned from:\n\t" + uri
     sys.exit(1)
 
 
-
-userInput = ""
-while True:
-  display = ["\n" for x in xrange(0,50)]
-  display.append(("EXPLORATION" if exploring else "NEIGHBOR VISUALIZATION") + " mode.\n")
+  userInput = ""
+  while True:
+    display = ["\n" for x in xrange(0,50)]
+    display.append(("EXPLORATION" if exploring else "NEIGHBOR VISUALIZATION") + " mode.\n")
+    
+    if exploring:
+      display.append("With the blue selection tool, click on the URI nodes to explore them.\n")
+      display.append("V for neighbor visualization mode.")
+    else:
+      display.append("With the blue selection tool, click on any node to visualize its neigbours.\n")
+      display.append("E for recursive exploration mode.")
+    display.append("L to toggle labels\nQ to quit.")
   
-  if exploring:
-    display.append("With the blue selection tool, click on the URI nodes to explore them.\n")
-    display.append("V for neighbor visualization mode.")
-  else:
-    display.append("With the blue selection tool, click on any node to visualize its neigbours.\n")
-    display.append("E for recursive exploration mode.")
-  display.append("L to toggle labels\nQ to quit.")
-
-  for s in display:
-    print s
-
-  userInput = raw_input().lower()
-  if userInput == "q" or userInput == "quit":
-    tulipgui.tlp.closeAllViews()
-    sys.exit(0)
-
-  elif userInput[0] == "l" and userInput in "labels":
-    showLabels = not showLabels
-    displayLabels(showLabels)
-
-  elif userInput[0] == "e" and userInput in "exploration" and not exploring:
+    for s in display:
+      print s
   
-    hide()
-    cont.restoreColors(nbrClicked)
-    show()
-    arrangeViews()
-
-    nbrClicked = []
-    exploring = True
-
-  elif userInput[0] == "v" and userInput in "visualization" and exploring:
-    exploring = False
+    userInput = input().lower()
+    if userInput == "q" or userInput == "quit":
+      tulipgui.tlp.closeAllViews()
+      sys.exit(0)
+  
+    elif userInput[0] == "l" and userInput in "labels":
+      showLabels = not showLabels
+      displayLabels(showLabels)
+  
+    elif userInput[0] == "e" and userInput in "exploration" and not exploring:
+    
+      hide()
+      cont.restoreColors(nbrClicked)
+      show()
+      arrangeViews()
+  
+      nbrClicked = []
+      exploring = True
+  
+    elif userInput[0] == "v" and userInput in "visualization" and exploring:
+      exploring = False
